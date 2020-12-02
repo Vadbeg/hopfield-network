7ad7e00378f0e6e189c5a6872e903c22bc84b7ef
Vadim Titko <vadbeg@tut.by>
2020-12-02 17:14:10 +0300

    Add config

---
+++ config.py
@@ -0,0 +1,12 @@
+"""Module with config class"""
+
+
+class Config:
+    """Config class"""
+
+    num_iter = 20
+    threshold = 50
+
+    image_size = (28, 28)
+
+    asynchronous = True
--- start_training.py
+++ start_training.py
@@ -7,6 +7,8 @@ from modules.data.dataset import Dataset
 from modules.network.hopfield import HopfieldNetwork
 from modules.utils.plots import plot_images_compare
 
+from config import Config
+
 
 def get_trained_model(image_paths: List[str],
                       image_size: Tuple[int, int] = (256, 256),
@@ -22,14 +24,18 @@ def get_trained_model(image_paths: List[str],
 
 def evaluation(model: HopfieldNetwork,
                image_paths: List[str],
-               image_size: Tuple[int, int] = (256, 256)):
+               image_size: Tuple[int, int] = (256, 256),
+               num_iter: int = 20,
+               threshold: int = 50):
 
     dataset_original = Dataset(list_of_paths=image_paths,
                                image_size=image_size,
                                add_noise=False)
     flatten_images_original = dataset_original.get_all_flatten_images()
 
-    predictions = model.predict(data=flatten_images_original, num_iter=20, threshold=50)
+    predictions = model.predict(data=flatten_images_original,
+                                num_iter=num_iter,
+                                threshold=threshold)
 
     data = {
         'original_image': flatten_images_original,
@@ -43,16 +49,14 @@ if __name__ == '__main__':
     image_paths_train = glob.glob(pathname='images_diff/train/*.*', recursive=True)
     image_paths_test = glob.glob(pathname='images_diff/test/*.*', recursive=True)
 
-    # image_paths = glob.glob(pathname='images_same/*.*', recursive=True)
-
-    image_size = (28, 28)
-
     model = get_trained_model(image_paths=image_paths_train,
-                              image_size=image_size,
-                              asynchronous=True)
+                              image_size=Config.image_size,
+                              asynchronous=Config.asynchronous)
 
     data = evaluation(model=model,
                       image_paths=image_paths_test,
-                      image_size=image_size)
+                      image_size=Config.image_size,
+                      num_iter=Config.num_iter,
+                      threshold=Config.threshold)
 
     plot_images_compare(data=data)
